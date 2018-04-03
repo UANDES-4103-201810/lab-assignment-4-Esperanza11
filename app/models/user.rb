a7812d9f9f35953e598dc1ff2bad1424b8808f02
ConiMarchant <cbmarchant@miuandes.cl>
2018-04-03 12:24:50 -0300

    Places are ready (maybe)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  	has_many :user_tickets	
-	has_many :tickets, :through => :user_tickets
+  	has_many :user_tickets
+    has_many :tickets, :through => :user_tickets
+    validates :phone, length: {in: 9..12 }, numericality: { only_integer: true },allow_nil: true
+    validates :password, length: {in: 8..12 }
+    validates :email, uniqueness: true
 end
