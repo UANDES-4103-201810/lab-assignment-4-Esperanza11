59729636215eca61424926c2f25c07f9e92e2dcd
ConiMarchant <cbmarchant@miuandes.cl>
2018-04-03 12:30:16 -0300

    Users ready (maybe)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :tickets, :through => :user_tickets
     validates :phone, length: {in: 9..12 }, numericality: { only_integer: true },allow_nil: true
     validates :password, length: {in: 8..12 }
-    validates :email, uniqueness: true
+    validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
 end
