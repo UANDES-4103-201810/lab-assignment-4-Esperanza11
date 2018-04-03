2539de24ba99bb3ff939ce9176afb6cea991ce84
ConiMarchant <cbmarchant@miuandes.cl>
2018-04-03 14:11:38 -0300

    Almost lol

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :tickets
-  validates :start_date, presence: true
+  validate :unique_start_date_and_place
+  def unique_start_date_and_place
+    events = Event.includes(:place).where("place_id = ? AND start_date = ?",self.place, self.start_date).references(:place).exists?
+    if !(events)
+      errors.add(:start_date, "There is another event in this place and date")
+    end
+  end
+
 end
